950f8c0ce6126083c191641accc97956abfc7221
ricksterhd123 <ricksterhd123@gmail.com>
2023-07-06 20:45:38 +0100

    Finish leet 21

--- c/leet21.c
+++ c/leet21.c
@@ -7,27 +7,11 @@ struct ListNode
     struct ListNode *next;
 };
 
-struct ListNode *push(struct ListNode *list, int val)
+struct ListNode *ListNode(int val)
 {
-    if (list == NULL)
-    {
-        list = (struct ListNode *)malloc(sizeof(struct ListNode));
-        list->val = val;
-        list->next = NULL;
-    }
-    else
-    {
-        struct ListNode *tmp = list;
-
-        while (tmp->next != NULL)
-        {
-            tmp = tmp->next;
-        }
-
-        tmp->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        tmp->next->val = val;
-        tmp->next->next = NULL;
-    }
+    struct ListNode *list = (struct ListNode *)malloc(sizeof(struct ListNode));
+    list->val = val;
+    list->next = NULL;
     return list;
 }
 
@@ -46,13 +30,38 @@ struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
     struct ListNode *tmp1 = list1;
     struct ListNode *tmp2 = list2;
     struct ListNode *result = NULL;
+    struct ListNode *tmpr = NULL;
 
-    while (tmp1 || tmp2) {
-        if (!tmp1 || (tmp2 && (tmp1->val > tmp2->val))) {
-            result = push(result, tmp2->val);
+    while (tmp1 || tmp2)
+    {
+        if (tmp2 && !tmp1 || tmp1->val > tmp2->val)
+        {
+            if (!result)
+            {
+                result = ListNode(tmp2->val);
+                tmpr = result;
+            }
+            else
+            {
+                tmpr->next = ListNode(tmp2->val);
+                tmpr = tmpr->next;
+            }
+
             tmp2 = tmp2->next;
-        } else {
-            result = push(result, tmp1->val);
+        }
+        else
+        {
+            if (!result)
+            {
+                result = ListNode(tmp1->val);
+                tmpr = result;
+            }
+            else
+            {
+                tmpr->next = ListNode(tmp1->val);
+                tmpr = tmpr->next;
+            }
+
             tmp1 = tmp1->next;
         }
     }
@@ -62,17 +71,23 @@ struct ListNode *mergeTwoLists(struct ListNode *list1, struct ListNode *list2)
 
 int main()
 {
+    // struct ListNode *t = NULL;
+    // struct ListNode *a;
+    // t = malloc(sizeof(struct ListNode));
+    // a = t;
+    // a->val = 2;
+    // printf("%d\n", t->val);
+
     printf("List1:\n");
-    struct ListNode *list1 = push(NULL, 1);
-    push(list1, 2);
-    push(list1, 4);
+    struct ListNode *list1 = ListNode(0);
+    list1->next = ListNode(2);
+    list1->next->next = ListNode(4);
     print(list1);
 
     printf("\nList2:\n");
-
-    struct ListNode *list2 = push(NULL, 1);
-    push(list2, 3);
-    push(list2, 4);
+    struct ListNode *list2 = ListNode(1);
+    list2->next = ListNode(3);
+    list2->next->next = ListNode(5);
     print(list2);
 
     printf("\nOutput:\n");
