a720760e5194101eec6d98f30028ece4d2ae8493
ricksterhd123 <ricksterhd123@gmail.com>
2023-07-12 21:31:08 +0100

    Solve part2 and tidy up

--- lua/aoc2022-4.lua
+++ lua/aoc2022-4.lua
@@ -1,13 +1,11 @@
 local data = require('aoc2022-4-data')
 
-function table.print(t)
-    local str = ""
-    for i, v in ipairs(t) do
-        str = str .. tostring(v) .. ((#t - i >= 1) and ", " or "")
-    end
-    str = str .. "\n"
-    return str
-end
+-- [[2-4,6-8
+-- 2-3,4-5
+-- 5-7,7-9
+-- 2-8,3-7
+-- 6-6,4-6
+-- 2-6,4-8]]
 
 function table.map(t, f)
     local k = {}
@@ -49,7 +47,7 @@ local function decodePair(pairstr)
     return { tonumber(x1), tonumber(y1), tonumber(x2), tonumber(y2) }
 end
 
-function isSubset(t)
+function isPairContained(t)
     local size1 = math.abs(t[1] - t[2])
     local size2 = math.abs(t[3] - t[4])
 
@@ -60,9 +58,21 @@ function isSubset(t)
     return t[1] <= t[3] and t[3] <= t[1] + size1 - size2
 end
 
+function isPairOverlapped(t)
+    local size1 = math.abs(t[1] - t[2])
+    local size2 = math.abs(t[3] - t[4])
+
+    if size1 < size2 then
+        return t[2] >= t[3] and t[1] <= t[4]
+    end
+
+    return t[4] >= t[1] and t[3] <= t[2] 
+end
+
 local paires = split(data, "\n")
 local decodedPairs = table.map(paires, decodePair)
-local subsetPairs = table.filter(decodedPairs, isSubset)
-local nSubsetPairs = #subsetPairs
+local containedPairs = table.filter(decodedPairs, isPairContained)
+local overlappedPairs = table.filter(decodedPairs, isPairOverlapped)
 
-print("Got "..tostring(nSubsetPairs) .." pairs containing eachother")
+print("Got "..tostring(#containedPairs) .." pairs containing eachother")
+print("Got "..tostring(#overlappedPairs) .." pairs overlapping eachother")
